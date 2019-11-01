90de0a979df07298b62f256ac5d394fc9e440997
Stephen Howe <Stevehowe10@gmail.com>
2019-10-31 18:09:25 -0600

    updating notes working

--- server.js
+++ server.js
@@ -102,6 +102,9 @@ app.get("/articles", function(req, res) {
   });
 });
 
+
+// DO THIS BUT FOR THE NOTES
+
 app.get("/delete", function(req, res){
   db.Article.remove({})  
   
@@ -297,18 +300,20 @@ app.get("/notes/:id", function(req, res) {
 });
 
 
-// // Route for grabbing a specific Article by id, populate it with it's note
-// app.delete("/notes/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   db.Note.findOne({ _id: req.params.id })
-//     // ..and populate all of the notes associated with it
-//     // .populate("note")
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find an Article with the given id, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+// DO THIS BUT FOR THE NOTES
+
+app.get("/notes", function(req, res){
+  db.Note.remove({})  
+  
+  .then(function(dbArticle) {
+     
+    // If we were able to successfully find Articles, send them back to the client
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    // If an error occurred, send it to the client
+    res.json(err);
+  });
+     // Send a message to the client
+     res.send("Clear Complete");
+})
